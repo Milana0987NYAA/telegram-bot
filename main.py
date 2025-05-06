1deb7080383b8966fac7e25ded80bf3673dcc353
МІЛКА <shivaisha84@gmail.com>
2025-05-06 17:15:56 +0300

    added basic functionality

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Messege
-
-bot = Bot(token='7850661812:AAFpzahnZC60IDy_lXyx2NBbkxHS6IyTNt4')
-dp = Dispatcher(bot)
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
 
 
+bot = Bot(token='7528668027:AAGWo1ZfnzGgpHiHNo5GQJbldM20AluE0B4')
+dp = Dispatcher()
 
-@dp.message()
-asynx def cmd_start(message: Message):
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
     await message.answer('Вітаю! Я бочонок, чим можу допомогти?')
     await message.reply('ЯК СПРАВИ?')
 
 
-asynx def main() :
+@dp.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer('Я ТУТ! Чим можу допомогти?')
+
+
+async def main():
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    print('Bot started')
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("бот зупиненор")
+    
